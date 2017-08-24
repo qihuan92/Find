524d4c54278f83ffa5bfa9ce1446d2d596f3fc0b
qihuan <qihuan@ejiaoji.com>
2017-08-24 15:07:52 +0800

    修改依赖文件，依赖room

--- app/src/main/java/com/qihuan/find/config/FindAppGlideModule.java
+++ app/src/main/java/com/qihuan/find/config/FindAppGlideModule.java
@@ -4,6 +4,6 @@ import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.module.AppGlideModule;
 
 @GlideModule
-public final class MyAppGlideModule extends AppGlideModule {
+public final class FindAppGlideModule extends AppGlideModule {
 
 }
--- app/src/main/java/com/qihuan/find/model/net/Client.java
+++ app/src/main/java/com/qihuan/find/model/net/Client.java
@@ -1,7 +1,5 @@
 package com.qihuan.find.model.net;
 
-import android.support.annotation.NonNull;
-
 import com.blankj.utilcode.util.NetworkUtils;
 import com.blankj.utilcode.util.Utils;
 import com.franmontiel.persistentcookiejar.PersistentCookieJar;
@@ -13,7 +11,6 @@ import com.qihuan.find.model.net.api.DoubanApi;
 import com.qihuan.find.model.net.api.ZhihuApi;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.Cache;
@@ -38,25 +35,22 @@ public class Client {
     private static int cacheSize = 10 * 1024 * 1024; // 10 MiB
     private static Cache cache = new Cache(httpCacheDirectory, cacheSize);
 
-    private static final Interceptor REWRITE_CACHE_CONTROL_INTERCEPTOR = new Interceptor() {
-        @Override
-        public Response intercept(@NonNull Chain chain) throws IOException {
-            Response originalResponse = chain.proceed(chain.request());
-            if (NetworkUtils.isAvailableByPing()) {
-                int maxAge = 60; // 在线缓存在1分钟内可读取
-                return originalResponse.newBuilder()
-                        .removeHeader("Pragma")
-                        .removeHeader("Cache-Control")
-                        .header("Cache-Control", "public, max-age=" + maxAge)
-                        .build();
-            } else {
-                int maxStale = 60 * 60 * 24 * 28;// 离线时缓存保存4周
-                return originalResponse.newBuilder()
-                        .removeHeader("Pragma")
-                        .removeHeader("Cache-Control")
-                        .header("Cache-Control", "public, only-if-cached, max-stale=" + maxStale)
-                        .build();
-            }
+    private static final Interceptor REWRITE_CACHE_CONTROL_INTERCEPTOR = chain -> {
+        Response originalResponse = chain.proceed(chain.request());
+        if (NetworkUtils.isAvailableByPing()) {
+            int maxAge = 60; // 在线缓存在1分钟内可读取
+            return originalResponse.newBuilder()
+                    .removeHeader("Pragma")
+                    .removeHeader("Cache-Control")
+                    .header("Cache-Control", "public, max-age=" + maxAge)
+                    .build();
+        } else {
+            int maxStale = 60 * 60 * 24 * 28;// 离线时缓存保存4周
+            return originalResponse.newBuilder()
+                    .removeHeader("Pragma")
+                    .removeHeader("Cache-Control")
+                    .header("Cache-Control", "public, only-if-cached, max-stale=" + maxStale)
+                    .build();
         }
     };
 

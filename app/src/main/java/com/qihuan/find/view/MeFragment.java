3254c83c2dfd2886b138c97eb1ec71949fe412f9
qihuan <qihuan@ejiaoji.com>
2017-09-23 15:09:44 +0800

    添加'我的'页面

--- app/src/main/java/com/qihuan/find/view/MeFragment.java
+++ app/src/main/java/com/qihuan/find/view/MeFragment.java
@@ -25,7 +25,7 @@ public class MeFragment extends BaseFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_news, container, false);
+        return inflater.inflate(R.layout.fragment_me, container, false);
     }
 
 }

4991f7423e1f40631eb54e2cfaa00087da423159
qihuan <qihuan@ejiaoji.com>
2018-04-19 15:28:22 +0800

    修改模块名称

---
+++ ModuleDaily/src/test/java/com/qihuan/dailymodule/presenter/DailyPresenterTest.java
@@ -0,0 +1,24 @@
+package com.qihuan.dailymodule.presenter;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DailyPresenterTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getLatestDaily() {
+    }
+
+    @Test
+    public void getBeforeDaily() {
+    }
+}

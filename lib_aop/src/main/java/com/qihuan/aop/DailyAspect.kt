fba133e26150ece50319bc202bda1fd56d40acbc
qihuan <qihuan@fengbangleasing.com>
2018-12-21 17:06:00 +0800

    修改名称

--- lib_aop/src/main/java/com/qihuan/aop/DailyAspect.kt
+++ lib_aop/src/main/java/com/qihuan/aop/DailyAspect.kt
@@ -6,17 +6,17 @@ import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
 
 /**
- * DailyAop
+ * DailyAspect
  * @author qi
  * @date 2018/12/21
  */
 @Aspect
-class DailyAop {
+class DailyAspect {
 
     @Around("execution(* com.qihuan.dailymodule.view.DailyFragment.onViewCreated(..))")
     fun onViewCreatedAround(joinPoint: ProceedingJoinPoint) {
-        Log.e("DailyAop", "DailyFragment::onViewCreated ---> in before")
+        Log.e("DailyAspect", "DailyFragment::onViewCreated ---> in before")
         joinPoint.proceed()
-        Log.e("DailyAop", "DailyFragment::onViewCreated ---> in after")
+        Log.e("DailyAspect", "DailyFragment::onViewCreated ---> in after")
     }
 }
